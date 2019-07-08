d943fdf8e1deac6105d6ec066f84da364b55c4f9
Billy Chen <billy.chen1206@gmail.com>
2019-07-08 13:42:11 -0400

    really sad forgot to push and lost it all

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source "https://rubygems.org" do
-  gem "sinatra"
   gem "sinatra-activerecord"
+  gem "sqlite3"
   gem "rake"
-end 
+end
--- server.rb
+++ server.rb
@@ -3,16 +3,59 @@ require "sinatra"
 
 ActiveRecord::Base.establish_connection(adapter: "sqlite3", database: "./database.sqlite3")
 
-enabled :sessions
+set :database, {adapter: "sqlite3", database: "./database.sqlite3"}
+
+enable :sessions
 
 class User < ActiveRecord::Base
 end
 
 get '/' do
+  puts "Running"
   erb :home
 end
 
-get './signup' do
+get '/signup' do
   @user = User.new
-  erb: 'users/signup'
+  erb :'users/signup'
+end
+
+post '/signup' do
+  @user = User.new(params)
+  if @user.save
+    p "#{@user.first_name} was saved to the database."
+    redirect '/thanks'
+  end
+end
+
+get '/thanks' do
+  erb :'users/thanks'
+end
+
+get '/login' do
+  if session[:user_id]
+    redirect '/'
+  else
+  erb :'users/login'
+  end
+end
+
+post '/login' do
+  given_password = params['password']
+  user = User.find_by(email: params['email'])
+  if user
+    if user.password == given_password
+      p 'User authenticated successfully!'
+      session[:user_id] = user.id
+    else
+      p "Invalid email or password"
+    end
+  end
+end
+
+# delete request
+post '/logout' do
+  session.clear
+  p 'User logged out successfully'
+  redirect '/'
 end
